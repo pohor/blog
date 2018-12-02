1297158f52c4e7602fd7f3cc467a61a39be35c8d
pohor <ola.pohorecka@gmail.com>
2018-12-02 12:18:23 +0100

    Assign comments routes to articles routes & amend the comment_controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
   # patch 'articles/:id', to: 'articles#update'
   # delete 'articles/:id', to: 'articles#destroy' - resources generuje to wszystko
 
-  resources :articles
-  resources :comments
-
+  resources :articles do
+    resources :comments
+  end
+  
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
