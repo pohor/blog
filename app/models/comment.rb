c1899ebacf5eaed7cd7e0b8fe81b3ea36b1791ea
pohor <ola.pohorecka@gmail.com>
2018-12-15 09:12:55 +0100

    Add email validation for commenter field

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :commenter, presence: true
-  validates :body, presence: true, length: { in: 6..500 }
+  validates :commenter, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :body, presence: true, length: 6..500
   belongs_to :article
 end
