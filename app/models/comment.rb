f4d3a0058f74e54e875e5ef0d8ab6282dad8fd3c
pohor <ola.pohorecka@gmail.com>
2018-12-15 09:22:43 +0100

    Email verification for commenter included in commenter model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
-  validates :commenter, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  include ActiveModel::Validations
+
+  validates :commenter, presence: true, email: true
   validates :body, presence: true, length: 6..500
   belongs_to :article
 end
